4162c850217878a726887f797e84517064d829a4
Chris Montgomery <christopher.montgomery@dowjones.com>
2014-10-30 23:50:10 -0500

    do not require PEM_KEY and DRIVE_USER in development. Fixes #2

--- spreadsheet.js
+++ spreadsheet.js
@@ -1,12 +1,33 @@
 var bluebird = require("bluebird");
 var Spreadsheet = require('edit-google-spreadsheet');
+var fs = bluebird.promisifyAll(require('fs'));
+var path = require('path');
 
 bluebird.promisifyAll(Spreadsheet);
 
 module.exports = function (worksheetName) {
-  return Spreadsheet.loadAsync(getSpreadSheetOptions(worksheetName))
-    .then(sheetReady)
-    .spread(processSpreadsheet)
+  if (process.env.NODE_ENV === 'production') {
+    return Spreadsheet.loadAsync(getSpreadSheetOptions(worksheetName))
+      .then(sheetReady)
+      .spread(processSpreadsheet)
+      .catch(function(err) {
+        console.error(err);
+        throw err;
+      });
+  }
+  // when running locally, return fake test data so you don't need PEM_KEY and DRIVE_USER
+  return getTestData(worksheetName);
+};
+
+function getTestData(worksheetName) {
+  var testDataMap = {
+    'Players': 'test/data/players-spreadsheet.json',
+    'Scores': 'test/data/scores-spreadsheet.json'
+  };
+  return fs.readFileAsync(path.join(__dirname, testDataMap[worksheetName]), 'utf8')
+    .then(function(jsonString) {
+      return JSON.parse(jsonString);
+    })
     .catch(function(err) {
       console.error(err);
       throw err;
@@ -19,8 +40,8 @@ function getSpreadSheetOptions(worksheetName){
     spreadsheetId: "1oT_G_vwbiYuh17TJhdsymFp8-ONbDMS2NckTGJtCZOE",
     worksheetName: worksheetName,
     oauth: {
-      email: "780272229048-had28ms0egrbau5vsk8nh330jbukos9j@developer.gserviceaccount.com", //process.env.DRIVE_USER,
-      keyFile: "./private/pongalytics.pem" //process.env.PEM_KEY
+      email: process.env.DRIVE_USER,
+      key: process.env.PEM_KEY
     }
   }
 }
